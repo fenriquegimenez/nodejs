07cbf9d1cf98f3e850e65031dbcbfe6e8137d129
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-01-06 14:11:12 -0300

    Primera escucha del servidor

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => res.send('Hola!'));
+
+app.listen(3000);
+
+console.log('La aplicación está escuchando en http://localhost:3000.');
